22f241ba4fb62bd4e42850eabc391be203f95f1e
Nguyen Duong <muoi07052001@gmail.com>
2023-05-27 11:39:35 +0700

    Add code to fetch document properties into the task pane's script objects

--- src/commands/commands.js
+++ src/commands/commands.js
@@ -43,8 +43,43 @@ async function toggleProtection(args) {
       // This code uses the worksheet object's protection property in a standard toggle pattern
       const sheet = context.workbook.worksheets.getActiveWorksheet();
 
+      /**
+       * In each function that you've created in this tutorial until now, you queued commands to write to the
+       * Office document. Each function ended with a call to the context.sync() method, which sends the
+       * queued commands to the document to be executed. However, the code you added in the last step
+       * calls the sheet.protection.protected property. This is a significant difference from the earlier
+       * functions you wrote, because the sheet object is only a proxy object that exists in your task pane's
+       * script. The proxy object doesn't know the actual protection state of the document, so its
+       * protection.protected property can't have a real value. To avoid an exception error, you must first
+       * fetch the protection status from the document and use it set the value of
+       * sheet.protection.protected. This fetching process has three steps:
+       *    1. Queue a command to load (that is; fetch) the properties that your code needs to read.
+       *    2. Call the context object's sync method to send the queued command to the document for
+       *       execution and return the requested information.
+       *    3. Because the sync method is asynchronous, ensure that it has completed before your code calls
+       *       the properties that were fetched.
+       */
+
       // 2. Queue command to load the sheet's "protection.protected" property from
       // the document and re-synchronize the document and task pane.
+
+      /**
+       * Every Excel object has a load method. You specify the properties of the object that you
+       * want to read in the parameter as a string of comma-delimited names. In this case, the
+       * property you need to read is a subproperty of the protection property. You reference the
+       * subproperty almost exactly as you would anywhere else in your code, with the exception
+       * that you use a forward slash ('/') character instead of a "." character.
+       */
+
+      /**
+       * To ensure that the toggle logic, which reads sheet.protection.protected, doesn't run
+       * until after the sync is complete and the sheet.protection.protected has been assigned
+       * the correct value that is fetched from the document, it must come after the await
+       * operator ensures sync has completed.
+       */
+      sheet.load("protection/protected");
+      await context.sync();
+
       if (sheet.protection.protected) {
         sheet.protection.unprotect();
       } else {
